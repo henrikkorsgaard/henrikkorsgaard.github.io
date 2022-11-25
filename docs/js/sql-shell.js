25646dd406a22e7b373fe379e7c8583d1d49556d
Henrik Korsgaard <korsgaard@cs.au.dk>
2022-11-25 01:17:44 +0100

    updating sql-shell

--- docs/js/sql-shell.js
+++ docs/js/sql-shell.js
@@ -1,21 +1,103 @@
-//Just design it as a good old module, but not as a module
+class SQLShell {
+    self = "hap"
 
-async function initShell(parentElement){
-    const sqlPromise = initSqlJs({
-        locateFile: file => `/js/sql/${file}`
-    });
-    const dataPromise = fetch("/data/publications.sqlite.db").then(res => res.arrayBuffer());
-    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
-    const db = new SQL.Database(new Uint8Array(buf));
-    var stmt = db.prepare("SELECT * FROM publications;");
-    stmt.getAsObject()
-    stmt.bind();
-    while(stmt.step()){
-        var row = stmt.getAsObject()
-        for(let k in row){
-            console.log(row[k])
+    static html = `<div class="sqlshell" id="output"></div>
+    <div class="sqlshell" id="prompt">sqlite></div><input class="sqlshell" type="text">`
+
+    static style = `<style>
+
+    div.sqlshell, input.sqlshell {
+        margin:auto;
+        font-family: monospace;
+        color:yellow;
+        font-size:16px !important; 
+        background: #2b1d1d;
+        text-align:left;
+        padding:5px !important;
+        box-sizing: border-box !important;
+    }
+    div#output.sqlshell {
+        width:100%;
+        height:100%;
+        padding:5px;
+        overflow-y:scroll;
+        display: flex;
+        flex-direction: column-reverse;
+    }
+    input.sqlshell {
+        vertical-align: top;
+        display:inline-block;
+        width:90%;
+        height: 25px;
+        border:none;
+    }
+    input.sqlshell:focus {
+        outline: none;
+    }
+
+    div#prompt.sqlshell {
+        vertical-align:top;
+        display: inline-block;
+        margin-top:-5px !important;
+        width:10%;
+        height:30px;
+        font-size:16px !important;
+    } 
+    </style>`
+
+    constructor(parent, dbfile){
+        this.parent = parent 
+        this.dbfile = dbfile
+
+        this.initDB()
+        this.parent.innerHTML = SQLShell.html
+        this.parent.innerHTML += SQLShell.style
+        
+        this.input = this.parent.querySelector('input[type="text"]')
+        this.output = this.parent.querySelector('#output')
+        this.history = []
+        
+        this.input.value = ""
+        this.input.addEventListener("keydown", this.inputHandler.bind(this))
+    }
+
+    inputHandler(e){
+        if(e.keyCode == 13){
+            let result = "sqlite> " + this.input.value + "<br>"
+                //We definitely need a validator here
+                if(this.input.value.trim() == ""){
+                    this.output.innerHTML += result;
+                    return
+                }
+                var stmt = this.db.prepare(this.input.value);
+                stmt.getAsObject()
+                stmt.bind();
+                while(stmt.step()){
+                    var row = stmt.getAsObject()
+                    for(let k in row){
+                        result += row[k] + "|"
+                    }
+                    result = result.substring(0, result.length-1)
+                    result +="<br>"
+                }
+                this.output.innerHTML += result;
+                this.history.push(this.input.value)
+                this.input.value = ""
+        }
+        if(e.keyCode == 38){
+            let last = this.history[this.history.length-1]
+            if(last){
+                this.input.value = last
+            }
         }
     }
-}
 
-initShell()
+    async initDB() {
+        const sqlPromise = initSqlJs({
+            locateFile: file => `/js/sql/${file}`
+        });
+        const dataPromise = fetch(this.dbfile).then(res => res.arrayBuffer());
+        const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+        this.db = new SQL.Database(new Uint8Array(buf));
+    }
+}
